fbc3a5b258202aaad3dd6ce99c0642f8cb11fb22
gagask <kholkovskii@mail.ru>
2021-07-15 20:35:09 +0300

    mb final commit

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
 def test(environ, start_response):
-	body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
-	start_response('200 OK', [('Content-Type', 'text/plain'),('Content-Length', str(len(body)))])
+	body = ['\r\n'.join(environ['QUERY_STRING'].split('&')).encode()]
+	start_response('200 OK', [('Content-Type', 'text/plain')])
 	return body
