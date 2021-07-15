69c7b05fe27418716161f762940ad545aec34e7c
gagask <kholkovskii@mail.ru>
2021-07-15 19:46:05 +0300

    adasd

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/gagask/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+gunicorn --bind='0.0.0.0:8080' hello:test
