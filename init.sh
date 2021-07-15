b1aee50ae24f2ebe2a84d49e8fd55760b2b0b20c
gagask <kholkovskii@mail.ru>
2021-07-15 21:14:29 +0300

    wtf

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo ln -s /home/gagask/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 gunicorn --bind='0.0.0.0:8080' hello:test
